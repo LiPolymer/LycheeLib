bacc0adbf7aea07ea88cd2f6673cae37105004db
LiPolymer <fish@lipoly.ink>
2025-07-01 13:06:46 +0800

    chore: remove ClassIsland Debugger Plugin

--- LycheeLib.Debugger/Program.cs
+++ LycheeLib.Debugger/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace LycheeLib.Debugger;
+﻿using LycheeLib.LyricsProviders;
+
+namespace LycheeLib.Debugger;
 
 static class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("开始接收");
+        LycheeLyricsInstance iInstance = new LycheeLyricsInstance();
+        iInstance.LoadProvider(new LyricIslandProvider());
+        LycheeLyricsInstance lInstance = new LycheeLyricsInstance();
+        lInstance.LoadProvider(new LxMusicProvider());
+        iInstance.OnLyricsChanged += lyrics => {
+            Console.WriteLine("[I]" + lyrics[0]);
+            Console.WriteLine(" - " + lyrics[1]);
+        };
+        lInstance.OnLyricsChanged += lyrics => {
+            Console.WriteLine("[L]" + lyrics[0]);
+            Console.WriteLine(" - " + lyrics[1]);
+        };
     }
 }
