888dcaa267e0cfe0254240f03fb3537624a95ee6
LiPolymer <fish@lipoly.ink>
2026-08-26 15:47:10 +0800

    refactor: migrate to newer SAI sdk

--- LycheeLib.Island/SaiRegistry.cs
+++ LycheeLib.Island/SaiRegistry.cs
@@ -2,50 +2,42 @@
 using ClassIsland.Shared;
 using LycheeLib.Interface;
 using LycheeLib.Island.Models;
-using SuperAutoIsland.Interface;
-using SuperAutoIsland.Interface.MetaData;
-using SuperAutoIsland.Interface.MetaData.ArgsType;
+using SuperAutoIsland.Interface.Metadata;
 using SuperAutoIsland.Interface.Services;
+using SuperAutoIsland.Interface.Services.Automations;
 
 namespace LycheeLib.Island;
 
 public static class SaiRegistry {
     public static void Register() {
         ISaiServer server = IAppHost.GetService<ISaiServer>();
-        
-        server.RegisterBlocks("LycheeLib", new RegisterData
-        {
-            Actions = [],
-            Rules = [],
-            Data = [
-                new BlockMetadata {
-                    Id = "lychee.data.fetch",
-                    Name = "获取歌词",
-                    Icon = ("音符", FluentIcons.MusicNote1Regular),
-                    Args = new Dictionary<string,MetaArgsBase> {
-                        ["Line"] = new DropDownMetaArgs {
-                            Name = "",
-                            Type = MetaType.dropdown,
-                            Options = [
-                                ("主行", "0"),
-                                ("副行", "1")
-                            ]
-                        }
-                    },
-                    DropdownUseNumbers = true,
-                    InlineBlock = true,
-                    InlineField = true
-                }
-            ]
-        });
-        
-        server.RegisterDataGetter<SaiLyricsFetcherConfig>("lychee.data.fetch",data => {
-            if (data is not SaiLyricsFetcherConfig config) {
-                return Task.FromResult("???");
-            }
 
-            ILycheeLyrics service = IAppHost.GetService<ILycheeLyrics>();
-            return Task.FromResult(service.Lyrics.Count <= config.Line ? "未载入" : service.Lyrics[config.Line]);
-        });
+        server.RegisterBlocks("LycheeLib", it => it
+            .AddLabel("LycheeLib")
+            .AddBlock<LyricsFetcherBlock>()
+        );
+    }
+}
+
+public class LyricsFetcherBlock : DataBlockBase {
+    public override string Id { get => "lychee.data.fetch"; }
+    public override string Name { get => "获取歌词"; }
+    public override (string, string) Icon { get => ("音符",FluentIcons.MusicNote1Regular); }
+    public override string DataOutput { get => "String"; }
+    public override Type SettingsType { get => typeof(SaiLyricsFetcherConfig); }
+
+    public override void GetFields(FieldsRegister it) => it
+        .AddField("Line", BasicFields.Dropdown("", [
+            ("主行", "0"),
+            ("副行", "1")
+        ], true));
+
+    public override Task<object> Handler(object? data) {
+        if (data is not SaiLyricsFetcherConfig config) {
+            return Task.FromResult<object>("???");
+        }
+
+        ILycheeLyrics service = IAppHost.GetService<ILycheeLyrics>();
+        return Task.FromResult<object>(service.Lyrics.Count <= config.Line ? "未载入" : service.Lyrics[config.Line]);
     }
 }
